9aba90673917c8f421aafd2171cc80703a088673
asifalam515 <asibulalam515@gmail.com>
2025-12-22 14:16:26 +0600

    query 4 added

--- queries.sql
+++ queries.sql
@@ -17,3 +17,20 @@ from
 select * from vehicles v where not exists (SELECT *
     FROM bookings b
     WHERE b.vehicle_id = v.vehicle_id)
+
+-- Query 3: WHERE
+select * from vehicles v where status='available'
+
+-- Query 4: GROUP BY and HAVING
+select
+  v.vehicle_id,
+  v.name AS vehicle_name,
+  count(b.booking_id) as total_bookings
+from
+  vehicles v
+  join bookings b on v.vehicle_id = b.vehicle_id
+group by
+  v.vehicle_id,
+  v.name
+having
+  COUNT(b.booking_id) > 2;
