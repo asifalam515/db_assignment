c3b0e0a321ebe8a36daced1d3f903bd18b224d9b
asifalam515 <asibulalam515@gmail.com>
2025-12-22 13:59:31 +0600

    second  query added

--- queries.sql
+++ queries.sql
@@ -10,3 +10,10 @@ from
   bookings b
   inner join users u on b.user_id = u.id
   inner join vehicles v on b.vehicle_id = v.vehicle_id
+
+
+
+--   Query 2: EXISTS
+select * from vehicles v where not exists (SELECT *
+    FROM bookings b
+    WHERE b.vehicle_id = v.vehicle_id)
