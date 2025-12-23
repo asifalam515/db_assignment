12787c7ff57605026d7516eb4c9919ad62944716
asifalam515 <asibulalam515@gmail.com>
2025-12-23 14:55:45 +0600

    column name updated and query updated

--- queries.sql
+++ queries.sql
@@ -8,22 +8,20 @@ select
   b.status
 from
   bookings b
-  inner join users u on b.user_id = u.id
+  inner join users u on b.user_id = u.user_id
   inner join vehicles v on b.vehicle_id = v.vehicle_id
 
 
 
 --   Query 2: EXISTS
 select * from vehicles v where not exists (SELECT *
-    FROM bookings b
-    WHERE b.vehicle_id = v.vehicle_id)
-
+    from bookings b
+    where b.vehicle_id = v.vehicle_id)
 -- Query 3: WHERE
 select * from vehicles v where status='available'
 
 -- Query 4: GROUP BY and HAVING
 select
-  v.vehicle_id,
   v.name as vehicle_name,
   count(b.booking_id) as total_bookings
 from
